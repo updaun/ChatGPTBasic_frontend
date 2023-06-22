5c0bb91ce0e2d99a404b38e6f6e643f2ac4649cb
updaun <updauney@daum.net>
2023-06-22 21:59:15 +0900

    [feat] chat ui update
    - enter input
    - empty string exception
    - UI update

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
 const chatBox = document.querySelector('.chat-box');
 
-        const sendMessage = async () => {
-            const chatInput = document.querySelector('.chat-input input');
-            const chatMessage = document.createElement('div');
-            chatMessage.classList.add('chat-message');
-            chatMessage.innerHTML = `
-    <p>${chatInput.value}</p>
-  `;
-            chatBox.appendChild(chatMessage);
-            chatInput.value = '';
+    const sendMessage = async () => {
+        const chatInput = document.querySelector('.chat-input input');
 
-            const response = await fetch('http://localhost:3000/fortuneTell', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    message: chatInput.value
-                })
-            });
+        // if the input is empty, return
+        if (!chatInput.value.trim()) return;
 
-            const data = await response.json();
+        const chatMessage = document.createElement('div');
+        chatMessage.classList.add('chat-message');
+        chatMessage.innerHTML = `<p>${chatInput.value}</p>`;
+        chatBox.appendChild(chatMessage);
 
-            const astrologerMessage = document.createElement('div');
-            astrologerMessage.classList.add('chat-message');
-            astrologerMessage.innerHTML = `
-    <p class='assistant'>${data.assistant}</p>
-  `;
-            chatBox.appendChild(astrologerMessage);
-        };
+        const message = chatInput.value;
+        chatInput.value = '';
 
-        document.querySelector('.chat-input button').addEventListener('click', sendMessage);
+        const response = await fetch('http://localhost:3000/fortuneTell', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                message: chatInput.value
+            })
+        });
+
+        const data = await response.json();
+
+        const astrologerMessage = document.createElement('div');
+        astrologerMessage.classList.add('chat-message');
+        astrologerMessage.innerHTML = `<p class='assistant'>${data.assistant}</p>`;
+        chatBox.appendChild(astrologerMessage);
+    };
+
+    document.querySelector('.chat-input button').addEventListener('click', sendMessage);
+    
+    // Add event listener for 'Enter' key
+    document.querySelector('.chat-input input').addEventListener('keydown', function(e) {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            sendMessage();
+        }
+    });
