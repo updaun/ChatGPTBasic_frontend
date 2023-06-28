4026f9eaba417af0fd9e795bf71767d41feb0acb
updaun <updauney@daum.net>
2023-06-28 23:02:21 +0900

    [feat] disable input while waiting for a response

--- script.js
+++ script.js
@@ -31,6 +31,8 @@ let myDateTime = '';
         // if the input is empty, return
         if (!chatInput.value.trim()) return;
 
+        chatInput.disabled = true;
+
         const chatMessage = document.createElement('div');
         chatMessage.classList.add('chat-message', 'user');
         chatMessage.innerHTML = `<p>${chatInput.value}</p>`;
@@ -42,7 +44,7 @@ let myDateTime = '';
         const astrologerMessage = document.createElement('div');
         astrologerMessage.classList.add('chat-message', 'bot');
         astrologerMessage.innerHTML = `
-            <div class="loader" id="loader" style="display: block;" >
+            <div class="loader" id="loader" style="display: block; vertical-align: middle; line-height: px" >
                 <i class="fa fa-spinner fa-spin" style="display: inline-block; vertical-align: middle"></i>
                 <p style="display: inline-block; vertical-align: middle">답변을 작성중입니다.</p>
             </div>
@@ -50,7 +52,7 @@ let myDateTime = '';
         chatBox.appendChild(astrologerMessage);
 
         const message = chatInput.value;
-        chatInput.value = '';
+        chatInput.value = '답변 중에는 입력할 수 없습니다. 잠시만 기다려주세요.';
 
         const response = await fetch('http://localhost:3000/fortuneTell', {
             method: 'POST',
@@ -71,6 +73,9 @@ let myDateTime = '';
         assistantMessages.push(data.assistant);
         astrologerMessage.innerHTML = `<p class='assistant'>${data.assistant}</p>`;
         chatBox.appendChild(astrologerMessage);
+        chatInput.disabled = false;
+        chatInput.value = '';
+        chatInput.focus();
     };
 
     document.querySelector('.chat-input button').addEventListener('click', sendMessage);
